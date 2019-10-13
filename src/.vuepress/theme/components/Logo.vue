cb3b57272e26c8a754902977e94d80e4f63e446d
Sarim Abbas <sarim.stays@gmail.com>
2019-10-13 02:52:07 -0400

    dark mode logo

--- src/.vuepress/theme/components/Logo.vue
+++ src/.vuepress/theme/components/Logo.vue
@@ -1,35 +1,17 @@
 <template>
   <a href="/">
-    <img v-if="dark" src="/white.png" alt="logo" width="50" />
-    <img v-else src="/black.png" alt="logo" width="50" />
+    <picture>
+      <source srcset="/white.png" media="(prefers-color-scheme: dark)" />
+      <img src="/black.png" width="50px" />
+    </picture>
   </a>
 </template>
 
 <script>
 export default {
-  name: "logo",
-  data() {
-    return {
-      dark: true
-    };
-  },
-  created: function() {
-    // if (matchMedia("(prefers-color-scheme: dark)").matches) {
-    //   this.dark = true;
-    // } else {
-    //   this.dark = false;
-    // }
-  }
+  name: "logo"
 };
 </script>
 
 <style scoped>
-.img {
-  background-image: url("/white.png");
-  width: 50px;
-  height: 20px;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-}
 </style>
