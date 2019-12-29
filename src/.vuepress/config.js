926253e61301de7d1ab9b80d0beff7a6fe4ce206
Sarim Abbas <sarim.stays@gmail.com>
2019-12-29 13:48:57 +0500

    add rss feed

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,14 +1,29 @@
 module.exports = {
-    title: "S.A. -  Sarim Abbas",
-    description: "Sarim Abbas' personal website",
-    markdown: {
-        // options for markdown-it-anchor
-        anchor: { permalink: false },
-        // options for markdown-it-toc
-        toc: { includeLevel: [1, 2, 3] }
-        // extendMarkdown: md => {
-        //   // use more markdown-it plugins!
-        //   md.use(require('markdown-it-xxx'))
-        // }
-    }
+  title: "S.A. -  Sarim Abbas",
+  description: "Sarim Abbas' personal website",
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    // options for markdown-it-toc
+    toc: { includeLevel: [1, 2, 3] }
+    // extendMarkdown: md => {
+    //   // use more markdown-it plugins!
+    //   md.use(require('markdown-it-xxx'))
+    // }
+  },
+  plugins: [
+    [
+      "vuepress-plugin-rss",
+      {
+        base_url: "/", // required
+        site_url: "https://sarimabbas.com", // required
+        // filter posts
+        filter: frontmatter => {
+          return frontmatter.published;
+        },
+        // How many articles
+        count: Infinity
+      }
+    ]
+  ]
 };
